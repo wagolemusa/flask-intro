dc895daeecd6b97272bd81e3a5666c03114bb934
kisumutalent <homiemusa@gmail.com>
2018-04-02 09:58:18 +0300

    setting up project

---
+++ app.py
@@ -0,0 +1,16 @@
+from  flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return "Hello Refuge Wise"
+	
+
+@app.route('/welcome')
+def welcome():
+	return render_template("welcome.html")
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
