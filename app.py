3ffdf5c0f6d99e15032e5b9c7be4bfc6f21a8b27
kisumutalent <homiemusa@gmail.com>
2018-04-02 10:16:46 +0300

    adding coments in app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
+# import the flask class  from the flask template
 from  flask import Flask, render_template
 
+# create the application  object
 app = Flask(__name__)
 
+# use decorators  to link the function to  a url
 @app.route('/')
 def home():
-	return "Hello Refuge Wise"
-	
+	return "Hello Refuge Wise" # return  a string 
+
 
 @app.route('/welcome')
 def welcome():
-	return render_template("welcome.html")
+	return render_template("welcome.html") #return a template
 
+
+#start the server with the  'run()' method
 
 if __name__ == '__main__':
 	app.run(debug=True)
